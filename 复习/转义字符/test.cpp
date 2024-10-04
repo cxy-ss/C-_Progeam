6d5ca9c489df302331c4cb40a8e7a79a25bf3d52
csyss <2591417160@qq.com>
2024-10-04 16:05:28 +0800

    ✨ feat: 新增转义字符的使用测试。

---
+++ 复习/转义字符/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+    using namespace std;
+int main() {
+    cout << "Hello,world!\n";
+    cout << "Hello,world!" << endl;
+    cout << "a\tHELOO\n";
+    cout << "aaa\tHELOO\n";
+    cout << "aaaaa\tHELOO\n";
+    cout << "\a";
+    cout << "\\";
+    return 0;
+}
