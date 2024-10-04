aeffcd0dc8f37b81f6f355364f367c56075d8b32
csyss <2591417160@qq.com>
2024-10-04 16:06:29 +0800

    ✨ feat: 新增数据类型测试代码。

---
+++ 复习/dataType/test.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;                         // 声明命名空间
+
+int main() {
+    /*int a = 10;
+    short a2 = 32768;
+    float b = 3.14;
+    double c = 2.71828;
+    char d = 'A';
+    bool e = true;
+    string f = "Hello, world!";
+
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "c = " << c << endl;
+    cout << "d = " << d << endl;
+    cout << "e = " << e << endl;
+    cout << "f = " << f << endl;
+    cout << "a2 = " << a2 << endl;
+*/
+//科学计数法的使用
+   /* float PI = 3.1415926;
+    float a = 6e7;
+    cout << "PI =" << PI << endl;
+    cout << "a=" << a << endl;*/
+    //字符型变量及ASCII码值
+    char ch = 'A';
+    cout << "A:ASCII:" << (int)ch << endl;
+    return 0;
+}
