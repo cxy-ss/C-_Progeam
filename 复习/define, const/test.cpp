eae022a1b21694487223d0508495ac3e57b205da
csyss <2591417160@qq.com>
2024-10-04 16:08:39 +0800

    ✨ feat: 常量的定义

---
+++ 复习/define, const/test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+//#define DAY 7
+int main()
+{
+    //cout<<"there are "<<DAY<<" days in a week"<<endl;
+    const int MONTH = 30;
+    cout<<"there are "<<MONTH<<" days in a month"<<endl;
+    return 0;
+}
