4a5b342c7649b44eb8696d414f9c88d3030b51ac
csyss <2591417160@qq.com>
2024-10-05 10:42:40 +0800

    ✨ feat: switch语句的使用

---
+++ 复习/选择结构switch/test.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*int score;
+    cout << "please input the score of the movie."<<endl;
+    cin >> score;
+    switch(score){
+        case 10:
+        case 9:
+            cout<<"the movie is excellent.";
+            break;
+        case 8:
+        case 7:
+            cout<<"the movie is good.";
+            break;
+        case 6:
+            cout<<"the movie is right.";
+            break;
+        default:
+            cout<<"the movie is bad.";
+            break;
+    }*/
+    cout << 8;
+    return 0;
+}
